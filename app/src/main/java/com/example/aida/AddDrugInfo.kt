684348a5e054a4443340267c5e3d69d53bd38a49
Lin <omasajiri@gmail.com>
2021-08-05 08:56:03 +0100

    feat: gallery for image selection implemented

--- app/src/main/java/com/example/aida/AddDrugInfo.kt
+++ app/src/main/java/com/example/aida/AddDrugInfo.kt
@@ -1,10 +1,32 @@
 package com.example.aida
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.aida.databinding.ActivityAddDrugInfoBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -41,12 +63,144 @@ class AddDrugInfo : AppCompatActivity() {
                 cal.get(Calendar.DAY_OF_MONTH)
             ).show()
         }
+
+        binding.tvAddImage.setOnClickListener {
+            val pictureDialog = androidx.appcompat.app.AlertDialog.Builder(this)
+            pictureDialog.setTitle("Select Action")
+            val pictureDialogItems = arrayOf("Select photo from gallery", "Capture photo from camera" )
+            pictureDialog.setItems( pictureDialogItems ){
+                    _, which ->
+                when(which){
+                    0 -> choosePhotoFromGallery()
+                    1 -> takePhotoFromCamera()
+                }
+            }
+            pictureDialog.show()
+        }
+
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK){
+            if (requestCode == GALLERY) {
+                if (data != null){
+                    val contentURI = data.data
+                    try {
+                        val selectedImageBitmap =
+                            MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+
+                        binding.ivPlaceImage!!.setImageBitmap(selectedImageBitmap)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+
+                        Toast.makeText(this, "Fail to upload image", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else if (requestCode == CAMERA){
+                val thumbnail: Bitmap = data!!.extras!!.get("data") as Bitmap
+
+                binding.ivPlaceImage!!.setImageBitmap(thumbnail)
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.e("Cancelled", "Cancel")
+        }
+    }
+
+
 
 
     private fun updateDateInView() {
         val myFormat = "d MMM, yyyy"
         val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
         binding.etExpirationDate.setText(sdf.format(cal.time).toString())
+    }
+
+    private fun choosePhotoFromGallery(){
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            .withListener(object: MultiplePermissionsListener{
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()){
+                        val galleryIntent = Intent(
+                            Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        )
+                        startActivityForResult(galleryIntent, GALLERY)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermission()
+                }
+
+            }).onSameThread()
+            .check()
+    }
+
+    private fun takePhotoFromCamera(){
+
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            )
+            .withListener(object: MultiplePermissionsListener{
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()){
+                        val galleryIntent = Intent(
+                            MediaStore.ACTION_IMAGE_CAPTURE
+                        )
+                        startActivityForResult(galleryIntent, CAMERA)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermission()
+                }
+
+            }).onSameThread()
+            .check()
+    }
+
+
+    private fun showRationalDialogForPermission(){
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton("GO TO SETTING"){
+                _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel"){
+                dialog, _ ->
+                dialog.dismiss()
+            }.show()
+
+    }
+
+    companion object{
+        private const val GALLERY = 1
+
+        private const val CAMERA = 2
+
+        private const val IMAGE_DIRECTORY = "AidaDrugMarket"
     }
 }
